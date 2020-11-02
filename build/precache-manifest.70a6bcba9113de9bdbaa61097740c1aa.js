ee2cf0b97e84a96a8f3a8dbd56b7f4ff0f834b1b
OscarCanongo <a01730443@itesm.mx>
2020-11-02 13:50:39 -0600

    mapa

---
+++ build/precache-manifest.70a6bcba9113de9bdbaa61097740c1aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f61c2780450207be7630002b737a0780",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36a163cb6fd083f907b4",
+    "url": "/static/css/main.8ba7a252.chunk.css"
+  },
+  {
+    "revision": "38e78cfc7027d42fb14c",
+    "url": "/static/js/2.ce906350.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce906350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36a163cb6fd083f907b4",
+    "url": "/static/js/main.82877a94.chunk.js"
+  },
+  {
+    "revision": "1df3dc75fd0d4ffa2d3b",
+    "url": "/static/js/runtime-main.3b7e1bbc.js"
+  }
+]);
